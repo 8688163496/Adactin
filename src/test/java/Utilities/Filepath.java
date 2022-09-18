9753d249bcd8cb28b882c6ce16508f5d76817b47
775662 <sudhananda496@gmail.com>
2022-09-18 23:01:53 +0530

    first commit

--- src/test/java/Utilities/Filepath.java
+++ src/test/java/Utilities/Filepath.java
@@ -3,5 +3,6 @@ package Utilities;
 public class Filepath {
 
 	public static final String Chromepath = "C:\\Users\\sudha\\OneDrive\\Desktop\\Cucumberproject\\lib\\chromedriver.exe";
+	public static final String XLPath = "C:\\Users\\sudha\\OneDrive\\Desktop\\Cucumberproject\\src\\test\\resources\\Excel\\Adactin hotel.xlsx" ;
 
 }
